8ccdb6b111c5ba6424d1adda6f51642ef153da05
ferencmurcsik <ferenc.murcsik@gmail.com>
2022-04-25 23:01:59 +0200

    added galery page without upload

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -17,10 +17,11 @@ $lablec = array(
 
 $oldalak = array(
 	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Címlap', 'menun' => array(1,1)),
-	'tudnivalok' => array('fajl' => 'tudnivalok', 'szoveg' => 'Tudnivalók', 'menun' => array(1,1)
-
+	'tudnivalok' => array('fajl' => 'tudnivalok', 'szoveg' => 'Tudnivalók', 'menun' => array(1,1)),
+	'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galéria', 'menun' => array(1,1)
+//	'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galeria', 'menun' => array(1,1)),
 //	'bemutatkozas' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás', 'menun' => array(1,1)),
-//	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1),
+//	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)
 //	'cikkek' => array('fajl' => 'cikkek', 'szoveg' => 'Cikkek', 'menun' => array(1,1)),
 //    'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat', 'menun' => array(1,1)),
 //    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
