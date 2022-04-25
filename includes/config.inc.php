7dbc768a76077cbbadf219cad785db0c03709cba
ferencmurcsik <ferenc.murcsik@gmail.com>
2022-04-25 21:39:08 +0200

    added two pages, updated open image

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -17,8 +17,10 @@ $lablec = array(
 
 $oldalak = array(
 	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Címlap', 'menun' => array(1,1)),
+	'tudnivalok' => array('fajl' => 'tudnivalok', 'szoveg' => 'Tudnivalók', 'menun' => array(1,1)
+
 //	'bemutatkozas' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás', 'menun' => array(1,1)),
-	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)
+//	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1),
 //	'cikkek' => array('fajl' => 'cikkek', 'szoveg' => 'Cikkek', 'menun' => array(1,1)),
 //    'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat', 'menun' => array(1,1)),
 //    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
---
+++ templates/pages/tudnivalok.tpl.php
@@ -0,0 +1,16 @@
+<h2>AZ ÖRÖKBEFOGADÁS MENETE, ÖRÖKBEFOGADÁSI MEGÁLLAPODÁS</h2>
+<p>Tisztelt Hölgyem, Uram! Kedves leendő gazdi!</p>
+<p>Üdvözöljük Önt a FÖRI Önkormányzati Állatvédelmi Őrszolgálatánál, ahol a főváros közigazgatási területén talált kóbor és ott regisztrált, leadott állatokból fogadhat örökbe.</p>
+<p>
+Az örökbefogadás menete:
+	<ol>
+		<li>A beléptetést követően a várakozóhelyiségbe irányítják Önt, ahonnan az állatgondozó az örökbefogadó helyiségbe kíséri. Itt megismertetik Önnel azokat az általános feltételeket és tudnivalókat, melyek elfogadása nélkül nem áll módunkban ebet vagy más állatot örökbe adni.</li>
+		<li>Az állampolgár, aki megelőzően saját állatáról lemondott, ugyanazt az állatot a későbbiekben nem fogadhatja újra örökbe, ahogyan a Szolgálatnál található más állatot sem.</li>
+		<li>Rövid beszélgetés keretében tájékozódunk az állattartás szempontjából lényeges kérdésekről (életvitel, lakóhely, szokások, elképzelések), és ezek alapján közösen megpróbáljuk körvonalazni, milyen állat felelne meg Önnek. Ebben nagy segítségünkre lesznek az Ön által adott válaszok és az állatgondozók tapasztalata az itt lévő állatok viselkedésével kapcsolatban.</li>
+		<li>Ezt követően választhatja ki a megfelelő állatot, akit az Önt kísérő állatgondozó vezet ki a kennelből és kísér az örökbefogadó helyiségbe.</li>
+		<li>Eztán állítjuk ki – az Ön személyazonosító igazolványainak alapján – az oltási könyvet, kitöltjük a mikrochip – regisztrációs adatlapot és aláírjuk az örökbefogadási megállapodást. Számla ellenében ugyancsak ekkor egyenlítheti ki a szolgáltatási díjat is, mely az állat kezelésével (oltások, mikrochip, féreghajtó) kapcsolatos költségeket minimálisan fedezi.</li>
+		<li>Az örökbefogadástól számított két héten belül telefonos vagy személyes konzultációt biztosítunk Önnek az állat egészségügyi állapotával, viselkedésével kapcsolatban. A tőlünk kivitt állat tartási körülményeit az általunk megbízott személy az Ön lakcímén utólagosan ellenőrizheti.</li>	
+	</ol>
+</p>
+<p>Kedves leendő gazdi!</p>
+<p>Bízunk abban, hogy Ön a kiválasztott állatnak jó gazdája, az pedig Önnek hűséges társa lesz!</p>
