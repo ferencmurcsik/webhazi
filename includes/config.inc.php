09763e4e81048ae7af2ddf30fbbd5d3480a08fe9
ferencmurcsik <ferenc.murcsik@gmail.com>
2022-04-27 22:25:48 +0200

    added contact page + client side check

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -18,7 +18,8 @@ $lablec = array(
 $oldalak = array(
 	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Címlap', 'menun' => array(1,1)),
 	'tudnivalok' => array('fajl' => 'tudnivalok', 'szoveg' => 'Tudnivalók', 'menun' => array(1,1)),
-	'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galéria', 'menun' => array(1,1)
+	'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galéria', 'menun' => array(1,1)),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)
 //	'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galeria', 'menun' => array(1,1)),
 //	'bemutatkozas' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás', 'menun' => array(1,1)),
 //	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)
---
+++ js/main.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+	var kuld = document.getElementById("kuld");
+	if (kuld) {
+		kuld.disabled = false;
+	}
+};
+function ellenoriz() {
+	var rendben = true;
+	var fokusz = null;
+
+	var szoveg = document.getElementById("szoveg");
+	if (szoveg) {
+		if (szoveg.value.length == 0 || szoveg.value.length > 200 )   {
+			rendben = false;
+			szoveg.style.background = '#f99';
+
+		} else {
+			szoveg.style.background = '#9f9';
+		}
+	}
+
+	var nev = document.getElementById("nev");
+	if (nev) {
+		if (nev.value.length<5) {
+			rendben = false;
+			nev.style.background = '#f99';
+			fokusz = nev;
+		} else {
+			nev.style.background = '#9f9';
+		}
+	}
+
+	if (fokusz) {
+		fokusz.focus();
+	}
+
+	var kuld = document.getElementById("kuld");
+	if (kuld) {
+		kuld.disabled = !rendben;
+	}
+
+	return rendben;
+}
