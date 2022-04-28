4bf1e1d4ee29e8d61653555894c89f839ce3d562
ferencmurcsik <ferenc.murcsik@gmail.com>
2022-04-28 22:26:28 +0200

    added image upload logic

--- templates/pages/galeria.tpl.php
+++ templates/pages/galeria.tpl.php
@@ -2,6 +2,7 @@
     $MAPPA = './images/kutyak/';
     $TIPUSOK = array ('.jpg', '.png');
     $MEDIATIPUSOK = array('image/jpeg', 'image/png');
+	$MAXMERET = 2000*3000;
     // Alkalmazás logika:
     
     // adatok összegyűjtése:    
@@ -15,7 +16,40 @@
         }
     closedir($olvaso);
 
+
+
+	// Kepfeltöltés 
+    $uzenet = array(); 
+
+	$readyToRefresh = FALSE;
     // Megjelenítés logika:
+	if (isset($_POST['kuld'])) {
+	//print_r($_FILES);
+	foreach($_FILES as $fajl) {
+		if ($fajl['error'] == 4);   // Nem töltött fel fájlt
+		elseif (!in_array($fajl['type'], $MEDIATIPUSOK))
+			$uzenet[] = " Nem megfelelő típus: " . $fajl['name'];
+		elseif ($fajl['error'] == 1   // A fájl túllépi a php.ini -ben megadott maximális méretet
+					or $fajl['error'] == 2   // A fájl túllépi a HTML űrlapban megadott maximális méretet
+					or $fajl['size'] > $MAXMERET) 
+			$uzenet[] = " Túl nagy állomány: " . $fajl['name'];
+		else {
+			$vegsohely = $MAPPA.strtolower($fajl['name']);
+			if (file_exists($vegsohely))
+				$uzenet[] = " Már létezik: " . $fajl['name'];
+			else {
+				move_uploaded_file($fajl['tmp_name'], $vegsohely);
+				$uzenet[] = ' Ok: ' . $fajl['name'];
+				$readyToRefresh = TRUE;
+				}
+			}
+		} 
+	}
+	if ($readyToRefresh) {
+		$readyToRefresh = FALSE;
+		header("Refresh:0");
+	}
+    
 ?>
     <meta charset="utf-8">
 	<style type="text/css">
@@ -38,36 +72,7 @@
     }
     ?>
     </div>
-	<!-- Kepfeltöltés -->
-	<?php
-    // Alkalmazás logika:
-    $uzenet = array();   
-
-    // Űrlap ellenőrzés:
-    if (isset($_POST['kuld'])) {
-        //print_r($_FILES);
-        foreach($_FILES as $fajl) {
-            if ($fajl['error'] == 4);   // Nem töltött fel fájlt
-            elseif (!in_array($fajl['type'], $MEDIATIPUSOK))
-                $uzenet[] = " Nem megfelelő típus: " . $fajl['name'];
-            elseif ($fajl['error'] == 1   // A fájl túllépi a php.ini -ben megadott maximális méretet
-                        or $fajl['error'] == 2   // A fájl túllépi a HTML űrlapban megadott maximális méretet
-                        or $fajl['size'] > $MAXMERET) 
-                $uzenet[] = " Túl nagy állomány: " . $fajl['name'];
-            else {
-                $vegsohely = $MAPPA.strtolower($fajl['name']);
-                if (file_exists($vegsohely))
-                    $uzenet[] = " Már létezik: " . $fajl['name'];
-                else {
-                    move_uploaded_file($fajl['tmp_name'], $vegsohely);
-                    $uzenet[] = ' Ok: ' . $fajl['name'];
-                }
-            }
-        }        
-    }
-	
-    // Megjelenítés logika:
-	?>
+  
 	<?php     
 	$isUserLoggedin = FALSE;
 	if(isset($_SESSION['login'])) { 
@@ -84,18 +89,11 @@
         echo '</ul>';
     }
 ?>
-    <form action="galeria.php" method="post"
+    <form method="post"
                 enctype="multipart/form-data">
         <label>Első:
             <input type="file" name="elso" required>
-        </label>
- <!--       <label>Második:
-            <input type="file" name="masodik">
-        </label>
-        <label>Harmadik:
-            <input type="file" name="harmadik">
-        </label>        
-		-->     
+        </label>    
         <input type="submit" name="kuld">
       </form>    
 	<?php } else { ?>
