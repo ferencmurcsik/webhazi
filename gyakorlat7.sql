83177817f23eebba7c60d904b776220a2c59c2fb
ferencmurcsik <ferenc.murcsik@gmail.com>
2022-04-28 23:49:01 +0200

    Update gyakorlat7.sql

--- gyakorlat7.sql
+++ gyakorlat7.sql
@@ -3,16 +3,15 @@ CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 USE `gyakorlat7`;
 CREATE TABLE `felhasznalok` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `csaladi_nev` varchar(45) NOT NULL DEFAULT '',
-  `uto_nev` varchar(45) NOT NULL DEFAULT '',
-  `bejelentkezes` varchar(12) NOT NULL DEFAULT '',
-  `jelszo` varchar(40) NOT NULL DEFAULT ''
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `felhasznalok`
---
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `csaladi_nev` varchar(45) NOT NULL default '',
+  `uto_nev` varchar(45) NOT NULL default '',
+  `bejelentkezes` varchar(12) NOT NULL default '',
+  `jelszo` varchar(40) NOT NULL default '',
+  PRIMARY KEY  (`id`)
+)
+ENGINE = MYISAM
+CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 INSERT INTO `felhasznalok` (`id`, `csaladi_nev`, `uto_nev`, `bejelentkezes`, `jelszo`) VALUES
 (1, 'Családi_1', 'Utónév_1', 'Login1', 'd4b90f2dfafc736205a98bf3ae6541431bc77d8e'),
@@ -27,8 +26,8 @@ INSERT INTO `felhasznalok` (`id`, `csaladi_nev`, `uto_nev`, `bejelentkezes`, `je
 (10, 'Családi_10', 'Utónév_10', 'Login10', 'a477427c183664b57f977661ac3167b64823f366'),
 (11, 'Családi_11', 'Utónév_11', 'Login11', '6c7e2151d33968a02198f7e2073b1d7b9ee65ed9'),
 (12, 'Családi_12', 'Utónév_12', 'Login12', '0722b3651be10eeb8df39cced958b74a98d18ce3'),
-(13, 'cik', 'cikóra', 'cik', '7c4a8d09ca3762af61e59520943dc26494f8941b'),
-(14, 'Cikus', 'cikória', 'cikcik', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+(13, 'Kovacs', 'Pal', 'kalapal', '7c4a8d09ca3762af61e59520943dc26494f8941b'),
+(14, 'Varga', 'Tihamér', 'vargatih', '7c4a8d09ca3762af61e59520943dc26494f8941b');
 
 CREATE TABLE `messages` (
   `id` int(10) unsigned NOT NULL auto_increment,
@@ -41,15 +40,7 @@ ENGINE = MYISAM
 CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 INSERT INTO `messages` (`id`,`bejelentkezes`,`uzenet`,`idobelyeg` ) VALUES 
- (1,'Login1','finom a kakos csiga 1',NOW()),
- (2,'Login2','finom a kakos csiga 2',NOW()),
- (3,'Login3','finom a kakos csiga 3',NOW()),
- (4,'Login4','finom a kakos csiga 4',NOW()),
- (5,'Login5','finom a kakos csiga 5',NOW()),
- (6,'Login6','finom a kakos csiga 6',NOW()),
- (7,'Login7','finom a kakos csiga 7',NOW()),
- (8,'Login8','finom a kakos csiga 8',NOW()),
- (9,'Login9','finom a kakos csiga 9',NOW()),
- (10,'Login10','finom a kakos csiga 10',NOW()),
- (11,'Login11','finom a kakos csiga 11',NOW()),
- (12,'Login12','finom a kakos csiga 12',NOW());
+ (1,'Vendég','Nérő kutya nagyon kedves, köszönjük a segítséget!',NOW()),
+ (2,'Varga Tihamér','Diego a kedvencem!',NOW()),
+ (3,'Vendég','Nyitva vannak szerdánként is?',NOW()),
+ (4,'Kovacs Pal','Sok szeretet a szegény állatoknak :(',NOW());
